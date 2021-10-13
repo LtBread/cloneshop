610f422cdc032ac98eb84a20de8f446c5650e11c
Bread <primebox21@gmail.com>
2021-10-13 20:47:01 +0700

    create models Products and ProductCategory

--- products/models.py
+++ products/models.py
@@ -14,4 +14,4 @@ class Products(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE())
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
