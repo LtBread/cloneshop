84c2e3d25544814d3323e896ce07678e6ddb8c1b
Bread <primebox21@gmail.com>
2021-10-13 21:40:48 +0700

    added __str__ for models.py

--- products/models.py
+++ products/models.py
@@ -7,11 +7,17 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
 
+    def __str__(self):
+        return self.name
 
-class Products(models.Model):
+
+class Product(models.Model):
     name = models.CharField(max_length=256)
     image = models.ImageField(upload_to='products_images', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
