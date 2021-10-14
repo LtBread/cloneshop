67a1ee867157c75db5e51af93fcde059476813d3
Bread <primebox21@gmail.com>
2021-10-14 20:50:11 +0700

    added models to admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from products.models import ProductCategory, Products
+
 # Register your models here.
+
+admin.site.register(ProductCategory)
+admin.site.register(Products)
