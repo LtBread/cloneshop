a9e2c227365c645f054fef28f0fb8e538febbf85
Bread <primebox21@gmail.com>
2021-10-19 22:49:38 +0700

    added UserLoginForm

--- users/views.py
+++ users/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
+
 
 def login(request):
-    context = {'title': 'Cloneshop - Авторизация'}
+    form = UserLoginForm()
+    context = {
+        'title': 'CloneShop - Авторизация',
+        'form': form,
+    }
     return render(request, 'users/login.html', context)
 
 
 def registration(request):
-    context = {'title': 'Cloneshop - Регистрация'}
+    context = {'title': 'CloneShop - Регистрация'}
     return render(request, 'users/registration.html', context)
