3e28a231fbbbdbfafb80197d98f46965b305473c
Bread <primebox21@gmail.com>
2021-10-27 19:06:06 +0700

    create CREATE for admin users

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 
 def index(request):
@@ -10,7 +13,14 @@ def index(request):
 
 # Create
 def admin_users_create(request):
-    context = {'title': 'CloneShop - Создание пользователя'}
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'CloneShop - Создание пользователей', 'form': form}
     return render(request, 'admins/admin-users-create.html', context)
 
 
