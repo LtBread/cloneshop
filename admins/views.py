d802c216b1f5de8b62e3902711c9ddab5c8db2bb
Bread <primebox21@gmail.com>
2021-11-03 18:44:21 +0700

    overwritten get_context_data() method for UserIndexView class

--- admins/views.py
+++ admins/views.py
@@ -1,18 +1,27 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
-@user_passes_test(lambda u: u.is_staff)
-def index(request):
-    context = {'title': 'CloneShop - Админ панель'}
-    return render(request, 'admins/index.html', context)
+class UserIndexView(TemplateView):
+    model = User
+    template_name = 'admins/index.html'
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserIndexView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(UserIndexView, self).get_context_data(**kwargs)
+        context['title'] = 'Админ-панель - Главная'
+        return context
 
 
 # Create
@@ -33,19 +42,6 @@ class UserCreateView(CreateView):
         return context
 
 
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users_create(request):
-#     if request.method == 'POST':
-#         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('admins:admin_users'))
-#     else:
-#         form = UserAdminRegistrationForm()
-#     context = {'title': 'CloneShop - Создание пользователей', 'form': form}
-#     return render(request, 'admins/admin-users-create.html', context)
-
-
 # Read
 
 class UserListView(ListView):
@@ -60,15 +56,6 @@ class UserListView(ListView):
         context = super(UserListView, self).get_context_data(**kwargs)
         context['title'] = 'Админ-панель - Пользователи'
         return context
-
-
-# @user_passes_test(lambda u: u.is_staff)s
-# def admin_users(request):
-#     context = {
-#         'title': 'CloneShop - Пользователи',
-#         'users': User.objects.all(),
-#     }
-#     return render(request, 'admins/admin-users-read.html', context)
 
 
 # Update
@@ -89,24 +76,6 @@ class UserUpdateView(UpdateView):
         return context
 
 
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users_update(request, id):
-#     selected_user = User.objects.get(id=id)
-#     if request.method == 'POST':
-#         form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('admins:admin_users'))
-#     else:
-#         form = UserAdminProfileForm(instance=selected_user)
-#     context = {
-#         'title': 'CloneShop - Редактирование пользователя',
-#         'form': form,
-#         'selected_user': selected_user,
-#     }
-#     return render(request, 'admins/admin-users-update-delete.html', context)
-
-
 # Delete
 
 class UserDeleteView(DeleteView):
@@ -123,9 +92,3 @@ class UserDeleteView(DeleteView):
         success_url = self.get_success_url()
         self.object.save_delete()
         return HttpResponseRedirect(success_url)
-
-# @user_passes_test(lambda u: u.is_staff)
-# def admin_users_delete(request, id):
-#     user = User.objects.get(id=id)
-#     user.save_delete()
-#     return HttpResponseRedirect(reverse('admins:admin_users'))
